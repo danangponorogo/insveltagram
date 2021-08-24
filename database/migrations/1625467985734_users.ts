6f13267072c0a3d017b83a25644e2f81d8520c0c
Danang Ponorogo <danang.dandies@gmail.com>
2021-08-24 22:53:11 +0700

    fix default update_at

--- database/migrations/1625467985734_users.ts
+++ database/migrations/1625467985734_users.ts
@@ -17,8 +17,8 @@ export default class Users extends BaseSchema {
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: false })
+      table.timestamp('updated_at', { useTz: false })
     })
   }
 
